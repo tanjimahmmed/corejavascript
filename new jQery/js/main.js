5974c6ced11616e9b3d5ed4caabf77dcb866125b
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-12-04 13:45:20 +0600

    Add files via upload

--- new jQery/js/main.js
+++ new jQery/js/main.js
@@ -3,50 +3,93 @@
 
     jQuery(document).ready(function ($) {
         // $("button").click(function () {
-            // $("#center p:first").hide();
-            // $("div:first").hide();
-            // $("div:not(#right, #center)").hide();
-            // $("div:odd").hide();
-            // $("p:contains('center')").hide();
-            // $("div:has(b)").hide();
-            // $("p:has(b)").hide();
-            // $("ul li:nth-child(1)").hide();
-            // $("body div:nth-child(3)").hide();
-            // $("li:empty").hide();
-            // $("li:first").css("color", "red");
-            // $("#right").css("border", "3px solid red");
-            // $("#right p").css("color", "red");
-            // $("#right").css({"border" : "2px solid red" , "color" : "red"});
-            // $("p").html("<p>Name: Tanjim<p/> ");
-            // $("p").text("Twinkel Cats");
-            // $("p").html("<h1>You sick</h1>");
-            // $("h2").text("give the some text add");
-            // $("#left").css("color", "red").css("border", "1px solid #green").fadeOut("2000");
-            // $("p:first").toggle("para2");
-            // $("#center").fadeOut(2500);
-            // $("#right").fadeIn(2500);
-            // $("#right").fadeIn(2500).fadeOut(2500);
-            // $("#right").fadeTo(2500, 0.9);
-            // $("#right").fadeToggle(2500);
-            // $("#right").prepend("<h2>add a new heading</h2>");
-            // $("#right").append("<h2>add a new heading</h2>");
-            // $("#rigth").before("<h2> Add line in before after </h2>");
-            // $("#right p").replaceWith("<p>Add a line</p>");
-            // $("#right").remove();
-            // $("div").remove();
-            // $("img").attr("src", "smile-2.png");
-            // $("img").removeAttr("src");
-            // $("img").mouseover(function () {
-            // $("p").text("all text are chenged");
-            // $("img").css("border", "1px solid red"); // anythink changed
-            // $("#right").append("<p>add new line</p>");
-            // $("#right").before("<p>add new line</p>");
-            // $("#right").after("<p>add new line</p>");
-            $("#right").hover(function (){
-            $("div").css("background-color",  "yellow");        
+        // $("#center p:first").hide();
+        // $("div:first").hide();
+        // $("div:not(#right, #center)").hide();
+        // $("div:odd").hide();
+        // $("p:contains('center')").hide();
+        // $("div:has(b)").hide();
+        // $("p:has(b)").hide();
+        // $("ul li:nth-child(1)").hide();
+        // $("body div:nth-child(3)").hide();
+        // $("li:empty").hide();
+        // $("li:first").css("color", "red");
+        // $("#right").css("border", "3px solid red");
+        // $("#right p").css("color", "red");
+        // $("#right").css({"border" : "2px solid red" , "color" : "red"});
+        // $("p").html("<p>Name: Tanjim<p/> ");
+        // $("p").text("Twinkel Cats");
+        // $("p").html("<h1>You sick</h1>");
+        // $("h2").text("give the some text add");
+        // $("#left").css("color", "red").css("border", "1px solid #green").fadeOut("2000");
+        // $("p:first").toggle("para2");
+        // $("#center").fadeOut(2500);
+        // $("#right").fadeIn(2500);
+        // $("#right").fadeIn(2500).fadeOut(2500);
+        // $("#right").fadeTo(2500, 0.9);
+        // $("#right").fadeToggle(2500);
+        // $("#right").prepend("<h2>add a new heading</h2>");
+        // $("#right").append("<h2>add a new heading</h2>");
+        // $("#rigth").before("<h2> Add line in before after </h2>");
+        // $("#right p").replaceWith("<p>Add a line</p>");
+        // $("#right").remove();
+        // $("div").remove();
+        // $("img").attr("src", "smile-2.png");
+        // $("img").removeAttr("src");
+        // $("img").mouseover(function () {
+        // $("p").text("all text are chenged");
+        // $("img").css("border", "1px solid red"); // anythink changed
+        // $("#right").append("<p>add new line</p>");
+        // $("#right").before("<p>add new line</p>");
+        // $("#right").after("<p>add new line</p>");
+        // $("#right").hover(function(){
+
+        //     $("div").css("background-color", "yellow");},
+
+        //     function(){
+
+        //         $("div").css("background-color", "gray");
+        //     });
+
+        // $("#right").hover(function(){
+        //     $("img").show();},
+        //     function(){
+        //         $("img").hide();
+        // });
+
+        // $("img").load(function(){
+        //     alert("the fun was loaded");
+        // });
+
+        // $(window).load(function () {
+        //     alert("the fun was loaded");
+        // });
+        // $("button").click(function(){
+        //     $("li").each(function(){
+        //         $(this).html("<li> on trigger change </li>");
+        //     });
+        // });
+
+        // $("button").click(function(){
+        //     $("#right").slideUp();
+        // });
+
+        // $("#testb").click(function(){
+        //     $("#right").slideDown();
+        // });
+
+        $("hidden_data").hide();
+
+        $("button").click(function(){
+            $(".hidden_data").slideUp();
+        });
+
+        $("#testb").click(function(){
+            $(".hidden_data").slideDown();
         });
 
     });
+
 
     jQuery(window).load(function () {
 
